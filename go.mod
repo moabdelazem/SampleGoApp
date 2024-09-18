28d78e650d9728ed35f86381a9d6b9b25d322f0b
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-09-18 22:00:58 +0300

    adding some code

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
 )
+
+require github.com/mattn/go-sqlite3 v1.14.23 // indirect
--- main.go
+++ main.go
@@ -1,28 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
-const (
-	// PORT is the port that the server will run on
-	PORT = ":8080"
-	// VERSION is the version of the API
-	VERSION = "v1"
-)
+const PORT string = ":8080"
+
+// Product Struct
+type Product struct {
+	ID            uuid.UUID `json:"id"`
+	Name          string    `json:"name"`
+	Price         string    `json:"price"`
+	Category      string    `json:"category"`
+	ProductNumber int       `json:"productNumber"`
+}
+
+func initRoutes(r *mux.Router) {
+	r.Use(mux.CORSMethodMiddleware(r))
+	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/products", ProductsHandler).Methods(http.MethodGet)
+}
+
+func JSONEncoder(w http.ResponseWriter, data interface{}, statusCode int) {
+	// Set Content Type and Status Code
+	w.Header().Set("Content-Type", "application/json")
+	// Set Status Code
+	w.WriteHeader(statusCode)
+	// Encode Data to JSON and Write to Response
+	json.NewEncoder(w).Encode(data)
+}
 
 func main() {
 	// Create Mux Router and Initialize Routes
 	r := mux.NewRouter()
+
 	// Initialize Routes
-	r.HandleFunc("/api/"+VERSION, func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to My API"))
-	}).Methods(http.MethodGet)
+	initRoutes(r)
 
 	log.Printf("Starting Server on Port %s", PORT)
 	// Start Server
 	log.Fatal(http.ListenAndServe(PORT, r))
+}
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hey, API!\n"))
+}
+
+/*
+ProductsHandler - Handler for Products
+- GET /products - Get All Products
+*/
+func ProductsHandler(w http.ResponseWriter, r *http.Request) {
+	products := []Product{
+		{ID: uuid.New(), Name: "Product 1", Price: "100", Category: "Category 1"},
+		{ID: uuid.New(), Name: "Product 2", Price: "200", Category: "Category 2"},
+		{ID: uuid.New(), Name: "Product 3", Price: "300", Category: "Category 3"},
+	}
+
+	JSONEncoder(w, products, http.StatusOK)
 }
